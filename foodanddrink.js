1e83a59675a4e4122a00f6ed6e8bba85863bd2a9
Tom Tung <cmtung.it@gmail.com>
2024-03-07 16:41:43 +0800

    added dailyintake object structure

--- foodanddrink.js
+++ foodanddrink.js
@@ -1,6 +1,39 @@
 // document.addEventListener("DOMContentLoaded", function () {});
 const nutritionAPI = "https://api.api-ninjas.com/v1/nutrition?query=";
 const recipeAPI = "https://api.api-ninjas.com/v1/recipe?query=";
+const dailyIntake = {
+  YM: {
+    age: "18-29.9",
+    gender: "male",
+    info: [{ calories: "1950" }],
+  },
+  AM: {
+    age: "30-59.9",
+    gender: "male",
+    info: [{ calories: "2350" }],
+  },
+  EM: {
+    age: ">=60",
+    gender: "male",
+    info: [{ calories: "2400" }],
+  },
+  YF: {
+    age: "18-29.9",
+    gender: "female",
+    info: [{ calories: "1700" }],
+  },
+  AF: {
+    age: "30-59.9",
+    gender: "female",
+    info: [{ calories: "1850" }],
+  },
+  EF: {
+    age: ">=60",
+    gender: "female",
+    info: [{ calories: "1900" }],
+  },
+};
+
 async function fetchNutritionAPI(input) {
   const response = await fetch(nutritionAPI + input, {
     headers: { "X-Api-Key": "fSlNftfk2M+xcXCj+72fPg==LtbIt40Y0yAjeaJ4" },
