84268b6904cc02afb945235a30a802074c175831
Tom Tung <cmtung.it@gmail.com>
2024-03-13 17:05:38 +0800

    nutrition part finished

--- foodanddrink.js
+++ foodanddrink.js
@@ -1,4 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {});
 const nutritionAPI = "https://api.api-ninjas.com/v1/nutrition?query=";
 const recipeAPI = "https://api.api-ninjas.com/v1/recipe?query=";
 const dailyIntake = {
@@ -33,29 +32,83 @@ const dailyIntake = {
     calories: 1900,
   },
 };
-let totalfatMin = dailyIntake["placeholder"].calories * 0.2;
-let totalfatMax = dailyIntake["placeholder"].calories * 0.35;
-let saturatedfat = dailyIntake["placeholder"].calories * 0.1;
-let proteinMin = dailyIntake["placeholder"].calories * 0.1;
-let proteinMax = dailyIntake["placeholder"].calories * 0.15;
-let sodium = 2000;
-let potassiumMin = 2700;
-let potassiumMax = 3100;
-let carbohydrateMin = dailyIntake["placeholder"].calories * 0.55;
-let carbohydrateMax = dailyIntake["placeholder"].calories * 0.75;
-let fiber = 25;
-let sugar = dailyIntake["placeholder"].calories * 0.1;
+// let totalfatMinLimit = dailyIntake["placeholder"].calories * 0.2;
+// let totalfatMaxLimit = dailyIntake["placeholder"].calories * 0.35;
+// let saturatedfatLimit = dailyIntake["placeholder"].calories * 0.1;
+// let proteinMinLimit = dailyIntake["placeholder"].calories * 0.1;
+// let proteinMaxLimit = dailyIntake["placeholder"].calories * 0.15;
+// let sodiumLimit = 2000;
+// let potassiumMinLimit = 2700;
+// let potassiumMaxLimit = 3100;
+// let carbohydrateMinLimit = dailyIntake["placeholder"].calories * 0.55;
+// let carbohydrateMaxLimit = dailyIntake["placeholder"].calories * 0.75;
+// let fiberLimit = 25;
+// let sugarLimit = dailyIntake["placeholder"].calories * 0.1;
 
+document.addEventListener("DOMContentLoaded", function () {
+  const resultContainer = document.getElementById("result-container");
+  const submitButton = document.getElementById("submitButton");
+  submitButton.addEventListener("click", async function () {
+    resultContainer.innerHTML = ""; // clear
+    hideError();
+    const searchInput = document.getElementById("searchInput"); // get input
+    const searching = searchInput.value.trim();
+    searchInput.value = searching;
+    const regex = new RegExp(/^[a-zA-Z0-9 ]*$/); // still in progress
+    regex.test(searching)
+      ? await fetchNutritionAPI(searching)
+      : showError("Please input correctly.");
+  });
+});
 
+async function fetchNutritionAPI(input) {
+  const resultContainer = document.getElementById("result-container");
+  const nutritionList = document.createElement("ul");
+  const response = await fetch(nutritionAPI + input, {
+    headers: { "X-Api-Key": "fSlNftfk2M+xcXCj+72fPg==LtbIt40Y0yAjeaJ4" },
+  });
+  const result = await response.json();
+  if (result) {
+    for (let i = 0; i < result.length; i++) {
+      for (const [key, value] of Object.entries(result[i])) {
+        if (key === "cholesterol_mg") {
+          continue;
+        }
+        const li = document.createElement("li");
+        li.textContent = `${key.replace(/_/g, " ")}: ${value}`;
+        nutritionList.appendChild(li);
+      }
+      resultContainer.appendChild(nutritionList);
+    }
+  } else {
+    return Promise.reject("result not found.");
+  }
+}
 
-// async function fetchNutritionAPI(input) {
-//   const response = await fetch(nutritionAPI + input, {
-//     headers: { "X-Api-Key": "fSlNftfk2M+xcXCj+72fPg==LtbIt40Y0yAjeaJ4" },
-//   });
-//   const result = await response.json();
-//   console.log(result);
-// }
-// fetchNutritionAPI("1lb brisket and fries");
+function showError(err) {
+  const error = document.getElementById("error");
+  error.textContent = err;
+  error.style.display = "block";
+}
+function hideError() {
+  const error = document.getElementById("error");
+  error.textContent = "";
+  error.style.display = "none";
+}
+
+//deprecated code
+// fetchNutritionAPI("1lb brisket 100g fries");
+// const name = result[i].name;
+// const calories = result[i].calories;
+// const servingSize = result[i].serving_size_g;
+// const totalFat = result[i].fat_total_g;
+// const saturatedFat = result[i].fat_saturated_g;
+// const protein = result[i].protein_g;
+// const sodium = result[i].sodium_mg;
+// const potassium = result[i].potassium_mg;
+// const carbohydrates = result[i].carbohydrates_total_g;
+// const fiber = result[i].fiber_g;
+// const sugar = result[i].sugar_g;
 
 // async function fetchrecipeAPI(input) {
 //   const response = await fetch(recipeAPI + input, {
